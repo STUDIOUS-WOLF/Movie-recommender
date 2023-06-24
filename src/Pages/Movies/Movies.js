4c7a45c2543a26044a1216e9ba22aabd9e62100e
amit <amit@rosatech.in>
2023-06-24 14:54:13 +0530

    edited movies component

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Genres from "../../components/Genres/Genres";
 import SingleContent from "../../components/SingleContent/SingleContent";
 import CustomPagination from "../../components/Pagination/CustomPagination";
 
 const Movies = () => {
- 
   const [page, setPage] = useState(1);
   const [content, setContent] = useState([]);
   const [numOfPages, setNumOfPages] = useState();
@@ -13,7 +11,7 @@ const Movies = () => {
 
   const fetchMovies = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
     );
     setContent(data.results);
     setNumOfPages(data.total_pages);
