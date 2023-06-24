cb39df3bd9f45cc29fed938b1d2a4d308e79f518
amit <amit@rosatech.in>
2023-06-24 14:34:10 +0530

    initial commit

---
+++ src/Pages/Series/Series.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import SingleContent from "../../components/SingleContent/SingleContent";
+
+
+const Series = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+
+
+  const fetchSeries = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+    );
+    setContent(data.results);
+    setNumOfPages(data.total_pages);
+    // console.log(data);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSeries();
+    // eslint-disable-next-line
+  }, [genreforURL, page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Discover Series</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="tv"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      {numOfPages > 1 && (
+        <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+      )}
+    </div>
+  );
+};
+
+export default Series;
